ca742df3a76f09bd72d1baaee0f305f69377bfab
Rishabh Bajaj <bajajris@shernet.sheridancollege.ca>
2021-09-13 23:22:44 -0400

    login and product designs

--- MrsCleanCapstone/wwwroot/js/site.js
+++ MrsCleanCapstone/wwwroot/js/site.js
@@ -1,1 +1,7 @@
-﻿
+﻿//toggle
+const menutoggle = document.querySelector('.toggle');
+const navigation = document.querySelector('.navigation');
+menutoggle.onclick = () => {
+    menutoggle.classList.toggle('active')
+    navigation.classList.toggle('active')
+}
