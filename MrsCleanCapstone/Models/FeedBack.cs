71f4d19d156b0b68d1220143b840e853c75e2dc9
kabaridi <kabaridi@sheridancollege.ca>
2021-10-06 09:40:33 -0400

    feedback Class created

---
+++ MrsCleanCapstone/Models/FeedBack.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
+
+
+namespace MrsCleanCapstone.Models
+{
+    public class FeedBack 
+    {
+        
+    }
+}
