9da4cc7023db196e36f28da122f4ccbb5297c0d4
kabaridi <kabaridi@sheridancollege.ca>
2021-11-26 01:56:55 -0500

    Responsive css added to myAppointment page

--- MrsCleanCapstone/DTOs/AppointmentLookupDto.cs
+++ MrsCleanCapstone/DTOs/AppointmentLookupDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,10 @@ namespace MrsCleanCapstone.DTOs
 {
     public class AppointmentLookupDto
     {
+        [DisplayName("Email")]
         public String CustomerEmail { get; set; }
 
+        [DisplayName("Enter Appointment ID")]
         public Guid AppointmentId { get; set; }
     }
 }
